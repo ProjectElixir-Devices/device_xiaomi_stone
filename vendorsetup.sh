5894d2d4054a1d7791ba622ed4495f03249596a7
Arijit78 <sahaarijit2007@gmail.com>
2024-02-28 14:29:26 +0000

    stone: track vendor from elixir org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ rm -rf device/xiaomi/sm6375-common
 git clone https://github.com/ProjectElixir-Devices/device_xiaomi_sm6375-common -b udc device/xiaomi/sm6375-common
 
 rm -rf vendor/xiaomi
-git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi-stone -b UNO vendor/xiaomi
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xioami_stone_unified -b UNO vendor/xiaomi
 
 rm -rf kernel/xiaomi/sm6375
 git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6375-stone -b 14.0 kernel/xiaomi/sm6375
