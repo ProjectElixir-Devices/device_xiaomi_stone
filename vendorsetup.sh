32f2208e9aa7181b6254f234853c2ce8729ba43c
Arijit78 <sahaarijit2007@gmail.com>
2024-03-09 15:03:42 +0000

    stone: Project Elixir BringUp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,18 @@
 # Clone Prebuilt Kernel
-git clone https://github.com/kamikaonashi/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
+rm -rf device/xiaomi/stone-kernel
+git clone https://github.com/ProjectElixir-Devices/device_xiaomi_stone-kernel.git -b UNO device/xiaomi/stone-kernel
 
 # Clone Vendor
-git clone https://github.com/kamikaonashi/vendor_xiaomi_stone_unified.git -b 14 vendor/xiaomi
+rm -rf vendor/xiaomi
+git clone https://github.com/ProjectElixir-Devices/vendor_xioami_stone_unified.git -b UNO vendor/xiaomi
 
 # Clone v4a
+rm -rf packages/apps/ViPER4AndroidFX
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
 
 # Clone hardware xiaomi
 git clone https://github.com/Evolution-X/hardware_xiaomi.git -b udc hardware/xiaomi
+
+# Clone Kernel source
+rm -rf kernel/xiaomi/sm6375
+git clone https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6375-stone.git -b 14.0 kernel/xiaomi/sm6375
