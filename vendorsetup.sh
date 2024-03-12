c96476b536ba7051cc9089477b61c75448f7834f
Arijit78 <sahaarijit2007@gmail.com>
2024-03-12 13:53:12 +0000

    Revert "stone: Track clang and add clang version"
    This reverts commit 0491eab67698ce1f0079f76f2b362e01b8f00940.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,7 +16,3 @@ git clone https://github.com/Evolution-X/hardware_xiaomi.git -b udc hardware/xia
 # Clone Kernel source
 rm -rf kernel/xiaomi/sm6375
 git clone https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6375-stone.git -b 14.0 kernel/xiaomi/sm6375
-
-# Kernel clang stuff
-rm -rf prebuilts/clang/host/linux-x86/clang-r510928
-git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928.git -b 14.0 prebuilts/clang/host/linux-x86/clang-r510928
