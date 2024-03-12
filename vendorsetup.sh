7a49b07734e736e7b38bae71b7ab94f09a05ad2e
pure-soul-kk <krishnakripa34567@gmail.com>
2024-03-12 13:55:16 +0000

    stone: Clone Aosp clang
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,3 +16,7 @@ git clone https://github.com/Evolution-X/hardware_xiaomi.git -b udc hardware/xia
 # Clone Kernel source
 rm -rf kernel/xiaomi/sm6375
 git clone https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6375-stone.git -b 14.0 kernel/xiaomi/sm6375
+
+# Aosp 17.0.4 Clang
+rm -rf prebuilts/clang/host/linux-x86/r498229b
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/r498229b
